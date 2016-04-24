f419cddbe28e01da9f75c8b24a9b1cdb459b1f19
cutsea110 <cutsea110@gmail.com>
2016-04-24 11:23:24 +0900

    generate csproj

--- src/CS/JsonDotNet.hs
+++ src/CS/JsonDotNet.hs
@@ -40,6 +40,10 @@ import CS.Common (CSharp, getEndpoints)
 
 data GenerateCsConfig
     = GenerateCsConfig { namespace :: String
+                       , classCsName :: String
+                       , apiCsName :: String
+                       , enumCsName :: String
+                       , convCsName :: String
                        , classtemplate :: GenerateCsConfig -> IO String
                        , apitemplate ::  forall api.
                                          (HasForeign CSharp Text api,
@@ -56,6 +60,10 @@ data GenerateCsConfig
 
 def :: GenerateCsConfig
 def = GenerateCsConfig { namespace = "ServantClientAPI"
+                       , classCsName = "Classes.cs"
+                       , apiCsName = "API.cs"
+                       , enumCsName = "Enum.cs"
+                       , convCsName = "JsonConverter.cs"
                        , classtemplate = defClassTemplate
                        , apitemplate = defAPITemplate
                        , enumtemplate = defEnumTemplate
@@ -543,3 +551,66 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 |]
+
+--------------------------------------------------------------------------
+
+defCsprojTemplate conf = do
+  guid <- maybe ((map toUpper . toString) <$> UUID.nextRandom) return $ guid conf
+  return [heredoc|<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="14.0" DefaultTargets="Build" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <Import Project="$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props" Condition="Exists('$(MSBuildExtensionsPath)\$(MSBuildToolsVersion)\Microsoft.Common.props')" />
+  <PropertyGroup>
+    <Configuration Condition=" '$(Configuration)' == '' ">Debug</Configuration>
+    <Platform Condition=" '$(Platform)' == '' ">AnyCPU</Platform>
+    <ProjectGuid>{${guid}}</ProjectGuid>
+    <OutputType>Library</OutputType>
+    <AppDesignerFolder>Properties</AppDesignerFolder>
+    <RootNamespace>${namespace conf}</RootNamespace>
+    <AssemblyName>${namespace conf}</AssemblyName>
+    <TargetFrameworkVersion>v4.5.2</TargetFrameworkVersion>
+    <FileAlignment>512</FileAlignment>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Debug|AnyCPU' ">
+    <DebugSymbols>true</DebugSymbols>
+    <DebugType>full</DebugType>
+    <Optimize>false</Optimize>
+    <OutputPath>bin\Debug\</OutputPath>
+    <DefineConstants>DEBUG;TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <PropertyGroup Condition=" '$(Configuration)|$(Platform)' == 'Release|AnyCPU' ">
+    <DebugType>pdbonly</DebugType>
+    <Optimize>true</Optimize>
+    <OutputPath>bin\Release\</OutputPath>
+    <DefineConstants>TRACE</DefineConstants>
+    <ErrorReport>prompt</ErrorReport>
+    <WarningLevel>4</WarningLevel>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="Newtonsoft.Json, Version=4.5.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed, processorArchitecture=MSIL" />
+    <Reference Include="System" />
+    <Reference Include="System.Core" />
+    <Reference Include="System.Xml.Linq" />
+    <Reference Include="System.Data.DataSetExtensions" />
+    <Reference Include="Microsoft.CSharp" />
+    <Reference Include="System.Data" />
+    <Reference Include="System.Net.Http" />
+    <Reference Include="System.Xml" />
+  </ItemGroup>
+  <ItemGroup>
+    <Compile Include="${apiCsName conf}" />
+    <Compile Include="${convCsName conf}" />
+    <Compile Include="${classCsName conf}" />
+    <Compile Include="${enumCsName conf}" />
+    <Compile Include="Properties\AssemblyInfo.cs" />
+  </ItemGroup>
+  <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
+  <!-- To modify your build process, add your task inside one of the targets below and uncomment it. 
+       Other similar extension points exist, see Microsoft.Common.targets.
+  <Target Name="BeforeBuild">
+  </Target>
+  <Target Name="AfterBuild">
+  </Target>
+  -->
+</Project>|]
